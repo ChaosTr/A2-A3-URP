d7b31e8eefa1b2edde97004574d2ad3d43f131db
ChaosTr <nakroth19081990@gmail.com>
2025-05-22 14:36:14 +0700

    devlog 22/05
    - Quick add to end scene

--- Assets/End.cs
+++ Assets/End.cs
@@ -1,15 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Hertzole.GoldPlayer;
 using UnityEngine.SceneManagement;
+
 
 public class End : MonoBehaviour
 {
+    public FadeInFadeOut fadeScript;
+    public GoldPlayerController playerScript;
+    public AudioSource busSound;
+    public bool isTriggered = false;
     public void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag("Player"))
+        if (other.CompareTag("Player") && !isTriggered)
         {
-            SceneManager.LoadScene("MainMenu");
+            StartCoroutine(EndScene());
+            isTriggered = true;
         }
+    }
+
+    private IEnumerator EndScene()
+    {
+        fadeScript.PassOut();
+        playerScript.enabled = false;
+        busSound.Play();
+        yield return new WaitForSeconds(5f);
+        playerScript.enabled = false;
+
     }
 }
